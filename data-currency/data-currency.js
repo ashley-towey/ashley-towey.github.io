8d7fcd5f9c60a3e6006af26e022c49b82fdf8201
Ashley Towey <toweyashley@gmail.com>
2022-05-09 21:46:41 +0100

    Update data-currency.js

--- data-currency/data-currency.js
+++ data-currency/data-currency.js
@@ -2,7 +2,7 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   textAlign(CENTER); //adjust the anchor point of text alignment to the horizontal and vertical centers
   textSize(20); //make the text 20 pixels in size
-  frameRate(30);
+  frameRate(10);
 }
 
 function draw() {
