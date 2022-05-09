e6db61e2ba79c9522d1f2576f947f4792c3b41a5
Ashley Towey <toweyashley@gmail.com>
2022-05-09 21:27:51 +0100

    updated p5 sketch transfer

--- data-currency/data-currency.js
+++ data-currency/data-currency.js
@@ -1,25 +1,28 @@
-// Where is the circle
-let x, y, speed;
+let content = 'WELCOME TO THE INFINITE SCROLL '; //variable for text string
+let yStart = 0; //starting position of the text wall
+let customFont; //variable for custom font
+
+function preload() {
+  customFont = loadFont('SpaceMono-Regular.ttf'); //use  preload to load the custom font
+}
 
 function setup() {
-  createCanvas(400, 400);
-  // Starts in the middle
-  x = width / 2;
-  y = height;
-  speed = 2;
+  createCanvas(windowWidth, windowHeight);
+  textFont(customFont); //use the custom font for text display
+  textAlign(CENTER, CENTER); //adjust the anchor point of text alignment to the horizontal and vertical centers
+  textSize(20); //make the text 20 pixels in size
+  //frameRate(20);
 }
 
 function draw() {
-  background(200);
-  
-  // Draw text
-  text('word', x - length, y);
-  
-  // Moving up at a constant speed
-  y = y - speed;
-  
-  // Reset to the bottom
-  if (y < 0) {
-    y = height;
+  background(0);
+  // random text
+  let words = ['Mail - Ashley Towey - Outlook', 'Netflix', 'Download My Virgin Media App | Virgin Media', '(179) Youtube', 'mismatch how design - Google Search', 'why do currency symbols have lines through them - Google Search', 'synonyms for personal - google search', 'Pinterest', 'pen plotter - Google Search', 'Training Expired ~ Central Saint Martins Loan Store ~ smarthub', 'Pinterest', 'Pinterest', 'Dashboard ~ Central Saint Martins Loan Store ~ smarthub', 'SM.00.H, Online Whiteboard for Visual Collaboration'];
+  let word = random(words); // select random word
+  for (let y = yStart; y < height; y += 28) { //use a for loop to draw the line of text multiple times down the vertical axis
+    //fill(255, y / 2 + 55, 100); //create a gradient by associating the fill color with the y location of the text
+    fill(255);
+    text(word, width / 2, y); //display text
   }
+  yStart--; //move the starting point of the loop up to create the scrolling animation, yStart-- is the same as yStart = yStart -1 or yStart-=1
 }
