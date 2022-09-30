4842a4c9740bfed7272c2f48c3db9fb758a0e68a
Ashley Towey <toweyashley@gmail.com>
2022-09-30 15:46:11 +0100

    testing read more for mutliple buttons

--- script.js
+++ script.js
@@ -14,3 +14,19 @@ function myFunction() {
     moreText.style.display = "inline";
   }
 }
+
+function vsMyFunction() {
+  var dots = document.getElementById("vsDots");
+  var moreText = document.getElementById("vsMore");
+  var btnText = document.getElementById("vsMyBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
