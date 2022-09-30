0d9b9f69102b92e100d60de7de725a7b2365f029
Ashley Towey <toweyashley@gmail.com>
2022-09-30 16:14:34 +0100

    broke 'read more'

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-/* read more function */
 function myFunction() {
   var dots = document.getElementById("dots");
   var moreText = document.getElementById("more");
@@ -15,18 +14,3 @@ function myFunction() {
   }
 }
 
-function vsMyFunction() {
-  var dots = document.getElementById("vsDots");
-  var moreText = document.getElementById("vsMore");
-  var btnText = document.getElementById("vsMyBtn");
-
-  if (dots.style.display === "none") {
-    dots.style.display = "inline";
-    btnText.innerHTML = "Read more"; 
-    moreText.style.display = "none";
-  } else {
-    dots.style.display = "none";
-    btnText.innerHTML = "Read less"; 
-    moreText.style.display = "inline";
-  }
-}
