a7a1bd405e64b485f104e100ef04aa9c6d091195
Ashley Towey <toweyashley@gmail.com>
2022-10-04 13:10:30 +0100

    first name working

--- scripts/not(!)-a-poem.js
+++ scripts/not(!)-a-poem.js
@@ -1,42 +1,10 @@
-// let firstName = document.getElementsByName("fName")[0].value;
-// let myName = 'Ashley';
-// let lastName = 'Towey';
 
-/* function formChanged() {
-    var firstName;
-    var lastName;
-}*/
-
-/* function myFunction(myName) {
-    console.log(myName + ' has passed the diploma in creative computing');
-    console.log('hello world');
-  }*/
-/*
-  let name = document.getElementsByName("fName")[0].value;
-  // let firstName = 'Ashley';
-  let lastName = 'Towey';
-
-  let x = 1;
-
-  function printmsg(){
-     let msg = firstName;
-     msg += " " + lastName;
-     msg += ", I've asked you "
-     msg += x+' times ';
-     msg += 'if you are ok!!';
-     let p = document.createElement("p");
-     p.innerHTML = msg;
-     document.getElementsByTagName("body")[0].appendChild(p);
-     x = x+1;
-  }*/
-
-  // display message function
   function displayMessage(){
-    let themsg = document.getElementById("msg").value;
-    if (themsg){
-        document.getElementById("showinputhere").innerHTML = themsg;
+    let theMsg = document.getElementById("firstName").value;
+    if (theMsg){
+        document.getElementById("showinputhere").innerHTML = theMsg;
     }
     else{
-        document.getElementById("showinputhere").innerHTML = "No message set";
+        document.getElementById("showinputhere").innerHTML = "No data received";
     }
 }
