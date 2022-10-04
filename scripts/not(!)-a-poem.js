ab4f62f96aff8f7c96c2596a2fa3c6254012557e
Ashley Towey <toweyashley@gmail.com>
2022-10-04 11:52:37 +0100

    javascript not working

---
+++ scripts/not(!)-a-poem.js
@@ -0,0 +1,12 @@
+// let firstName = document.getElementsByName("fName")[0].value;
+let firstName = 'Ashley';
+let lastName = 'Towey';
+
+/* function formChanged() {
+    var firstName;
+    var lastName;
+}*/
+
+function myName(firstName, lastName) {
+    console.log(firstName + lastName + ' has passed the diploma in creative computing');
+  }
