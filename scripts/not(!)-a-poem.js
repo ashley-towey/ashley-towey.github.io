5dcd17d86ce83d484091a30437f4c1c7365bf901
Ashley Towey <toweyashley@gmail.com>
2022-10-04 12:02:58 +0100

    still not working

--- scripts/not(!)-a-poem.js
+++ scripts/not(!)-a-poem.js
@@ -1,12 +1,30 @@
 // let firstName = document.getElementsByName("fName")[0].value;
-let firstName = 'Ashley';
-let lastName = 'Towey';
+// let myName = 'Ashley';
+// let lastName = 'Towey';
 
 /* function formChanged() {
     var firstName;
     var lastName;
 }*/
 
-function myName(firstName, lastName) {
-    console.log(firstName + lastName + ' has passed the diploma in creative computing');
+/* function myFunction(myName) {
+    console.log(myName + ' has passed the diploma in creative computing');
+    console.log('hello world');
+  }*/
+
+  const name = 'Bob';
+  let x = 1;
+  
+  function printmsg(){
+     let msg = name;
+     msg = "I've asked you "
+     msg += x+' times ';
+     msg += 'if you are ok!!';
+     let p = document.createElement("p");
+     p.innerHTML = msg;
+     document.getElementsByTagName("body")[0].appendChild(p);
+     x = x+1;
   }
+  printmsg();
+  printmsg();
+  printmsg();
