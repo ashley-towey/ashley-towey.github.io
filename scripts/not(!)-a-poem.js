461e9b0e16356673fa62f6d241825b23b1837480
Ashley Towey <toweyashley@gmail.com>
2022-10-04 13:03:27 +0100

    input and display messge

--- scripts/not(!)-a-poem.js
+++ scripts/not(!)-a-poem.js
@@ -11,20 +11,32 @@
     console.log(myName + ' has passed the diploma in creative computing');
     console.log('hello world');
   }*/
+/*
+  let name = document.getElementsByName("fName")[0].value;
+  // let firstName = 'Ashley';
+  let lastName = 'Towey';
 
-  const name = 'Bob';
   let x = 1;
-  
+
   function printmsg(){
-     let msg = name;
-     msg = "I've asked you "
+     let msg = firstName;
+     msg += " " + lastName;
+     msg += ", I've asked you "
      msg += x+' times ';
      msg += 'if you are ok!!';
      let p = document.createElement("p");
      p.innerHTML = msg;
      document.getElementsByTagName("body")[0].appendChild(p);
      x = x+1;
-  }
-  printmsg();
-  printmsg();
-  printmsg();
+  }*/
+
+  // display message function
+  function displayMessage(){
+    let themsg = document.getElementById("msg").value;
+    if (themsg){
+        document.getElementById("showinputhere").innerHTML = themsg;
+    }
+    else{
+        document.getElementById("showinputhere").innerHTML = "No message set";
+    }
+}
