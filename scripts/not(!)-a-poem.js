140dde43b2b91749c5014ea8d6cf7d5bbf37a9b7
Ashley Towey <toweyashley@gmail.com>
2022-10-05 20:57:17 +0100

    space styling

--- scripts/not(!)-a-poem.js
+++ scripts/not(!)-a-poem.js
@@ -13,7 +13,7 @@ setInterval(function() {
     let minutes = Math.floor((finalDate % (1000 * 60 *60)) / (1000 * 60));
     let seconds = Math.floor((finalDate % (1000 * 60)) / 1000);
 
-    counter.innerHTML = "has been alive for " +days+" days "+hours+" hours "+minutes+" minutes "+seconds+" seconds ";
+    counter.innerHTML = days+" days "+hours+" hours "+minutes+" minutes "+seconds+" seconds ";
 }, 1000) 
 }
 
@@ -22,7 +22,7 @@ function displayData(){
     let lName = document.getElementById("lastName").value;
 
     if (fName + lName){
-        document.getElementById("showinputhere").innerHTML = fName + " " + lName;
+        document.getElementById("showinputhere").innerHTML = fName + " " + lName + " has been alive for";
     } else {
         document.getElementById("showinputhere").innerHTML = "Not enough data provided.";
     }
