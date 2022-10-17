bfbe472a435389073dac7f35cc1e698ef2b5ff27
Ashley Towey <toweyashley@gmail.com>
2022-10-17 09:44:35 +0100

    creative computing links updated

--- scripts/playground.js
+++ scripts/playground.js
@@ -1,7 +1,15 @@
-function setup() {
-    // createCanvas(400, 400);
+const cursorRounded = document.querySelector('.rounded');
+// const cursorPointed = document.querySelector('.pointed');
+
+
+const moveCursor = (e)=> {
+  const mouseY = e.clientY;
+  const mouseX = e.clientX;
+   
+  cursorRounded.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+  
+  // cursorPointed.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+ 
 }
 
-function draw() {
-    // ellipse(mouseX, mouseY, 50);
-}
+window.addEventListener('mousemove', moveCursor)
