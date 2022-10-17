3d23b856551af9a3766d964d09fd1eaadf3c87f5
Ashley Towey <toweyashley@gmail.com>
2022-10-17 11:17:03 +0100

    testing safari/chrome

--- scripts/cci-week-4.js
+++ scripts/cci-week-4.js
@@ -1,8 +1,61 @@
-//alert('This is slightly cringe');
+/*
 function setup() {
     createCanvas(400, 400);
+    background(0);
+
+    rectMode(CENTER);
+    noStroke();
+    blendMode(DIFFERENCE);
+    fill (255);
+    rect (200, 200, 150);
+    ellipse(220, 220, 150);
+
 }
 
 function draw() {
-    background(200);
 }
+*/
+/* Coding Challenge #2
+Explore the blendMode() command by drawing a series of shapes 
+on top of each other. 
+
+Adjust their position on the canvas using the push() and pop() 
+commands and translate() | rotate().
+
+Hint: explore the commands blendMode(DIFFERENCE) and
+blendMode(BLEND)
+*/
+
+
+let x = 400;
+let y = 400;
+
+function setup() {
+createCanvas(800, 800);
+blendMode(BLEND);
+
+background(0);
+
+noStroke();
+
+blendMode(DIFFERENCE);
+
+fill(255);
+
+rectMode(CENTER);
+
+rect(x, y, 350);
+push();
+    translate(70, 80);
+    ellipse(x, y, 330);
+pop();
+
+push();
+translate(70, 80);
+triangle(0, 350, 350/2, 0, 350, 350);
+pop();
+}
+
+
+
+
