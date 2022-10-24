650a0412f8d6f63760b311c93981ab3eda0c1a7f
Ashley Towey <a.towey0220201@arts.ac.uk>
2022-10-24 12:21:40 +0100

    custom cursor working, but not with page contents

--- scripts/custom-cursor.js
+++ scripts/custom-cursor.js
@@ -1,6 +1,9 @@
 const cursorRounded = document.querySelector('.rounded');
 const cursorPointed = document.querySelector('.pointed');
 
+let prevX = null;
+let prevY = null;
+
 
 const moveCursor = (e)=> {
   const mouseY = e.clientY;
@@ -12,4 +15,37 @@ const moveCursor = (e)=> {
  
 }
 
-window.addEventListener('mousemove', moveCursor)
+window.addEventListener('mousemove', moveCursor);
+
+/*
+let prevX = null
+let prevY = null
+
+// How thick the lines should be
+ctx.lineWidth = 5
+
+window.addEventListener("mousemove", (e) => {
+    // initially previous mouse positions are null
+    // so we can't draw a line
+    if(prevX == null || prevY == null){
+        // Set the previous mouse positions to the current mouse positions
+        prevX = e.clientX
+        prevY = e.clientY
+        return
+    } 
+
+    // Current mouse position
+    let currentX = e.clientX
+    let currentY = e.clientY
+
+    // Drawing a line from the previous mouse position to the current mouse position
+    ctx.beginPath()
+    ctx.moveTo(prevX, prevY)
+    ctx.lineTo(currentX, currentY)
+    ctx.stroke()
+
+    // Update previous mouse position
+    prevX = currentX
+    prevY = currentY
+})
+*/
