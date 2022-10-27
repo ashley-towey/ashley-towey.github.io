516f982abf3e54f6a2783445200d8c8e0ef1c1e7
Ashley Towey <a.towey0220201@arts.ac.uk>
2022-10-27 16:25:43 +0100

    added arrow key control

--- scripts/cci-week-5-p5js.js
+++ scripts/cci-week-5-p5js.js
@@ -1,58 +1,51 @@
-/*let xPos = 5;
-let xStep = 15;
-
-function setup() {
-    createCanvas(windowWidth, windowHeight);
-    rectMode(CENTER);
-
-}
-
-function draw() {
-    background(200);
-
-    for(let i = 0; i < width/xStep; i++){
-        factor = i/10;
-        let yPos = map(noise(factor), 0, 1, 0, height); // use perlin noise for the y value
-        // rect(xPos+(xStep*i), yPos, xStep, xStep); // draw the rectangle
-        line (i, yPos, i, height);
-
-    }
-}
-*/
-/*
-let noiseScale=0.02;
-
-function draw() {
-  background(0);
-  for (let x=0; x < width; x++) {
-    let noiseVal = noise((mouseX+x)*noiseScale, mouseY*noiseScale); 
-    stroke(noiseVal*255);
-    line(x, mouseY+noiseVal*80, x, height);
-  }
-  /* describe(`horizontal wave pattern effected by mouse x-position
-    & updating noise values.`); 
-} */
-
 let noiseValue = 4;
+let bgFillCol = 0;
+let strokeCol = 255;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
+    noLoop();
   }
   
   function draw() {
-    background(0);
-    noFill();
-    stroke(255);
-  
-    
-    beginShape();
-    for (var x = -150; x < width+150; x++) {
-      var nx = map(x, 0, width, 0, noiseValue);
-      var y = height * noise(nx);
-      //vertex(x, y);
-      strokeWeight(.2);
-      ellipse(x, y, 300);
-    }
-    endShape();
-
+    resetSketch();
   }
+
+function keyPressed() {
+  if (keyCode === DOWN_ARROW){
+    noiseValue -= .5;
+  } else if (keyCode === UP_ARROW){
+    noiseValue += .5;
+  } else if (keyCode === RIGHT_ARROW) {
+    bgFillCol = 0;
+    strokeCol = 255;
+  } else if (keyCode === LEFT_ARROW) {
+    bgFillCol = 255;
+    strokeCol = 0;
+  }
+
+  console.log(noiseValue);
+  redraw();
+}
+
+function mousePressed() {
+  resetSketch();
+  console.log('mouse is pressed');
+}
+
+function resetSketch() {
+  background(bgFillCol);
+  noFill();
+  stroke(strokeCol);
+
+  beginShape();
+  for (var x = -150; x < width+150; x++) {
+    var nx = map(x, 0, width, 0, noiseValue);
+    var y = height * noise(nx);
+    //vertex(x, y);
+    strokeWeight(.2);
+    ellipse(x, y, 300);
+  }
+  endShape();
+
+}
