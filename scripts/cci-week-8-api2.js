bf9bb79c4cec549ad9e5dc356afa4122a9d28345
Ashley Towey <a.towey0220201@arts.ac.uk>
2022-11-14 12:19:33 +0000

    created api 2 too

---
+++ scripts/cci-week-8-api2.js
@@ -0,0 +1,7 @@
+function setup() {
+    createCanvas(400, 400); // oooooh a canvas that may or may not be square
+}
+
+function draw() {
+    background(230, 80, 80); // and a fucking background, fuck
+}
