f0125d31172dde8f8e88789d4daff16aaa1d9934
Ashley Towey <a.towey0220201@arts.ac.uk>
2023-04-20 13:30:28 +0100

    what 3 pubs lessgoo

--- what3pubs/main.js
+++ what3pubs/main.js
@@ -3,8 +3,20 @@ var sheet_name = 'what3pubs';
 var key = "AIzaSyB5b_wv4yQMDoHTCDDZydcbYxLZ5ISrGbQ"
 var url = 'https://sheets.googleapis.com/v4/spreadsheets/' + id + '/values/' + sheet_name + '?alt=json&key=' + key;
 
+function processData(rows) {
+  var headers = rows[0];
+  var content = document.getElementById('content');
+  rows.shift();
+  // console.log(headers, rows);
+
+  rows.forEach(row => {
+    // console.log(row);
+    content.innerHTML += "<li>"+row+"</li>";
+  });
+}
 //this gets the data from the google sheet
 fetch(url)
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(data => processData(data.values));
+
 
