6969a24512da164c1a35c852b8cb1259ff6aa9d8
Ashley Towey <a.towey0220201@arts.ac.uk>
2023-04-25 16:14:13 +0100

    segmenting the different pubs

--- what3pubs/main.js
+++ what3pubs/main.js
@@ -3,6 +3,7 @@ var sheet_name = 'what3pubs';
 var key = "AIzaSyB5b_wv4yQMDoHTCDDZydcbYxLZ5ISrGbQ"
 var url = 'https://sheets.googleapis.com/v4/spreadsheets/' + id + '/values/' + sheet_name + '?alt=json&key=' + key;
 
+
 function processData(rows) {
   var headers = rows[0];
   var content = document.getElementById('content');
@@ -10,13 +11,16 @@ function processData(rows) {
   // console.log(headers, rows);
 
   rows.forEach(row => {
-    // console.log(row);
-    content.innerHTML += "<li>"+row+"</li>";
+    console.log(row[4]);
+    content.innerHTML += "<h1>"+row[3]+"</h1>";
+    content.innerHTML += "<h3>"+"Location: "+row[4]+"</h3>";
+    content.innerHTML += "<p>" + "Price: " + row[5] + "</p>";
+    content.innerHTML += "<hr>";
+    
   });
 }
 //this gets the data from the google sheet
 fetch(url)
   .then(response => response.json())
   .then(data => processData(data.values));
-
 
