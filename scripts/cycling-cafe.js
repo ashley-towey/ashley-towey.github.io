72f93792dff4a779d4d88f0a0054a24db6df6962
Ashley Towey <a.towey0220201@arts.ac.uk>
2023-10-23 16:14:58 +0100

    synchronised

--- scripts/cycling-cafe.js
+++ scripts/cycling-cafe.js
@@ -3,16 +3,16 @@ let angle = 0;
 let speed = 0.03;
 
 // the center of our rotation:
-let centerX = 300;
-let centerY = 350;
+let centerX = 400;
+let centerY = 400;
 
 // the center of the 2nd rotation
-let centerX2 = 370;
-let centerY2 = 350;
+let centerX2 = 400;
+let centerY2 = 400;
 
 // the third rotation
-let centerX3 = 270;
-let centerY3 = 350;
+let centerX3 = 400;
+let centerY3 = 400;
 
 function setup() { 
   createCanvas(800, 800);
@@ -20,7 +20,12 @@ function setup() {
 }
 
 function draw() { 
-  background(255);
+
+  background(240);
+
+  stroke(0);
+  fill(255);
+  ellipse(width/2, height/2, 600);
   noStroke();
   // text(angle, 50, 50);
   
@@ -31,7 +36,7 @@ function draw() {
   // draw shape as though (centerX, centerY) is the new
   // origin / (0, 0) point
   fill(180, 10, 10, 180);
-  ellipse(radius-50, radius-30, 400);
+  ellipse(radius-35, radius-35, 450);
   pop();
   
   push();
@@ -39,17 +44,18 @@ function draw() {
   translate(centerX2, centerY2);
   rotate (angle);
   fill(10, 10, 180, 150);
-  ellipse(radius+30, radius+30, 300);
+  ellipse(radius, radius, 320);
   pop();
+
   push();
   // translate around the 2nd point / axis
   translate(centerX3, centerY3);
   rotate (angle);
   fill(255,255,0, 130);
-  ellipse(radius-20, radius-20, 200);
+  ellipse(radius+30, radius+30, 200);
   pop();
   // make it blurry
-  drawingContext.filter = 'blur(25px)';
+  //drawingContext.filter = 'blur(25px)';
 
   angle = angle + speed;
 }
