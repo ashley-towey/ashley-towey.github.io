0ad73f16a398f00c64288a5cb979971a36779109
Ashley Towey <a.towey0220201@arts.ac.uk>
2023-11-21 12:57:18 +0000

    testing upto 5.44

--- scripts/type-size.js
+++ scripts/type-size.js
@@ -1,3 +1,6 @@
+let button;
+let permissionGranted = false;
+
 function setup() {
     createCanvas(windowWidth, windowHeight);
 
@@ -5,10 +8,25 @@ function setup() {
     if (typeof(DeviceOrientationEvent) !== 'undefined' && typeof(DeviceOrientationEvent.requestPermission) === 'function') {
         // ios 13 device
         background(255, 0, 0);
+        button = createButton("click to allow access to sensors");
+        button.style("font-size", "24px");
+        button.center();
+        button.mousePressed( requestAccess );
+
     } else { 
         // non ios 13 device
         background(0, 255, 0);
     }
+}
+
+function requestAccess() {
+    DeviceOrientationEvent.requestPermission()
+    .then(response => {
+        if (response == 'granted') {
+            permissionGranted = true;
+        }
+    })
+    .cartch(console.error);
 }
 
 function draw() {
