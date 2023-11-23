2fcf5e210ed44449a2aa6b553bd6a675a294f1ff
Ashley Towey <a.towey0220201@arts.ac.uk>
2023-11-23 10:24:47 +0000

    aero-dynamic font working

--- scripts/aero-dynamic.js
+++ scripts/aero-dynamic.js
@@ -3,17 +3,24 @@ let slantSlider;
 let casualSlider;
 let variable;
 
+// load in the font
+let font;
+
+function preload() {
+  font = loadFont('../data/AeroDynamic_2VF.ttf');
+}
+
 function setup() {
   noCanvas()
   
-  weightSlider = createSlider(300, 1000, 300);
+  weightSlider = createSlider(0, 100, 50);
   weightSlider.style('width', '180px');
   
-  slantSlider = createSlider(-15, 0, 0);
+  slantSlider = createSlider(0, 100, 0);
   slantSlider.style('width', '180px');
   
-  casualSlider = createSlider(0, 1, 0, 0.01);
-  casualSlider.style('width', '180px');
+//   casualSlider = createSlider(0, 1, 0, 0.01);
+//   casualSlider.style('width', '180px');
   
   variable = select('.variable');
 }
@@ -23,5 +30,5 @@ function draw() {
   
   // variable.style('font-weight', 300+mouseX);
   variable.style('font-weight', weightSlider.value())
-  variable.style('font-variation-settings', "'slnt' " + slantSlider.value() + ", 'CASL' " + casualSlider.value());
+  variable.style('font-variation-settings', "'slnt' " + slantSlider.value());
 }
