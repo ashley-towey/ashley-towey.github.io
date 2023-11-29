5e9316e7fbc71532c17762e882b85e75775e72af
Ashley Towey <a.towey0220201@arts.ac.uk>
2023-11-29 18:28:43 +0000

    data sources and variable type working

--- scripts/dynamic-prototype-2.js
+++ scripts/dynamic-prototype-2.js
@@ -1,82 +1,48 @@
-let permissionGranted = false;
-let weight;
-let aero;
+let weightSlider;
+let slantSlider;
+let variable;
+
+// new variables that will hold the speed and gradient values
+let Speed;
+let Gradient;
+
+// variables that will control the typeface 
+let Weight;
+let Aero;
 
 // load in the font
-let font;
+// let font;
 
 function preload() {
   font = loadFont('../data/AeroDynamic_2VF.ttf');
 }
 
-// variable to affect the HTML DOM element type
-let variable;
-
 function setup() {
-    createCanvas(windowWidth, 500);
-    
-    variable = select('.variable');
+  noCanvas()
+  
+  variable = select('.variable');
 
-    // DeviceOrientationEvent, DeviceMotion Event 
-    if (typeof(DeviceOrientationEvent) !== 'undefined' && typeof(DeviceOrientationEvent.requestPermission) === 'function') {
-        // ios 13 device
-        DeviceOrientationEvent.requestPermission()
-        .catch(() => {
-            // show permission dialog only the first time
-        let button = createButton("click to allow access to sensors");
-        button.style("font-size", "24px");
-        button.center();
-        button.mousePressed( requestAccess );
-
-        throw error;
-        })
-        .then(() => {
-            // on any subsquent visits
-            permissionGranted = true;
-        })
-
-    } else { 
-        // non ios 13 device
-        textSize(48);
-        text("non ios 13 device", 100, 100);
-         }
-
-}
-
-function requestAccess() {
-    DeviceOrientationEvent.requestPermission()
-    .then(response => {
-        if (response == 'granted') {
-            permissionGranted = true;
-        } else {
-            permissionGranted = false;
-        }
-    })
-    .catch(console.error);
-
-    this.remove();
+  // set the starting point for Speed & Gradient
+  let Speed = 0;
+  let Gradient = 0;
 }
 
 function draw() {
-    background(255);
-
-    // rounding the numbers to make animation smooth
-    let roundedX = round(rotationX);
-    let roundedY = round(rotationY);
-
+  background(220);
+  
     // adding the sliders for the variable font
-    let weight = map(roundedX, -40, 40, 0, 100);
-    let aero = map(roundedY, -80, 0, 0, 100);
+    Aero = map(mouseX, 0, windowWidth, 0, 100);
+    Weight = map(mouseY, 0, windowHeight, 100, 0);
 
-    variable.style('font-weight', weight);
-    variable.style('font-variation-settings', "'slnt' " + aero);
+    // map the values of Aero and Weight to Speed and Gradient
+    Speed = map(Aero, 0, 100, 0, 40);
+    Gradient = map(Weight, 0, 100, -10, 10);
 
-    if (!permissionGranted) return;
+  // variable.style('font-weight', 300+mouseX);
+  variable.style('font-weight', Weight);
+  variable.style('font-variation-settings', "'slnt' " + Aero);
 
-    // // testing text below
-    // textSize(30);
-    // text("Rotation X: " + roundedX, 100, 100);
-    text("Rotation Y: " + roundedY, 100, 200);
-    text("weight: " + weight, 100, 300);
-    text("CDA: "+ aero, 100, 400);
+  document.getElementById("actual-speed").innerHTML = round(Speed)+" kph";
+  document.getElementById("actual-gradient").innerHTML = round(Gradient)+" %";
+
 }
