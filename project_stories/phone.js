8e3243feb32f62ae9d1bbd28cfcacd90aef7cbbf
Ashley Towey <a.towey0220201@arts.ac.uk>
2024-01-11 12:51:23 +0000

    please push update

--- project_stories/phone.js
+++ project_stories/phone.js
@@ -76,7 +76,7 @@ function draw() {
     let roundedY = round(rotationY);
 
     // mapping the variability from the orientation to the typeface values
-    let weight = map(roundedX, 0, 50, 0, 100);
+    let weight = map(roundedX, 0, 45, 0, 100);
     let aero = map(roundedY, -30, 30, 0, 100);
 
     variable.style('font-weight', weight);
