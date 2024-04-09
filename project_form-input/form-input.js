72955d70218d13e9fd226a472186f19d75cdaeed
Ashley Towey <a.towey0220201@arts.ac.uk>
2024-04-09 12:18:35 +0100

    prompt says success

---
+++ project_form-input/form-input.js
@@ -0,0 +1,21 @@
+window.addEventListener("load", function() {
+    const form = document.getElementById('my-form');
+    form.addEventListener("submit", function(e) {
+      e.preventDefault();
+      const data = new FormData(form);
+      const action = e.target.action;
+      fetch(action, {
+        method: 'POST',
+        body: data,
+      })
+      .then(() => {
+        alert("Success!");
+      })
+    });
+  });
+
+  console.log("Hello World");
+
+//   function formSubmitted() {
+//     window.location.href="../index.html";
+//   }
